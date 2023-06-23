3f69a98bfaa2e37fb0189d5105863a19a5563051
rpill <rpill@github.com>
2023-06-23 21:06:06 +0000

    fab4260844d8026b829cae2c1447d136414e911b

--- lib/utils/puppeteer.d.ts
+++ lib/utils/puppeteer.d.ts
@@ -1,4 +1,4 @@
 import { Page } from 'puppeteer';
 import { Options, LaunchResult } from '../types/utils/puppeteer';
 export declare const hasElementBySelectors: (page: Page, selectors: string) => Promise<boolean>;
-export declare const launchBrowser: (htmlPath: string, options: Options) => Promise<LaunchResult>;
+export declare const launchBrowser: (htmlPath: string, options?: Options) => Promise<LaunchResult>;
